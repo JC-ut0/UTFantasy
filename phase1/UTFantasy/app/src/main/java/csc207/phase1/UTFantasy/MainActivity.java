6a0d32032659fda9a1535ec0b969d5902a91a0cd
Yexiong Xu <yexiong.xu@mail.utoronto.ca>
2019-10-11 15:29:47 -0400

    # Created Phase1 project. # Name of the Project: UTFantasy

---
+++ phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/MainActivity.java
@@ -0,0 +1,14 @@
+package csc207.phase1.UTFantasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
