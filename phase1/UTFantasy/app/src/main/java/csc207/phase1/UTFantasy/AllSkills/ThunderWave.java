d2eacc8237c3a7226667189f396846c2623c1ba9
yimi <yimiswz.wang@mail.utoronto.ca>
2019-10-19 00:12:07 -0400

    Update ThunderWave

---
+++ phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/AllSkills/ThunderWave.java
@@ -0,0 +1,11 @@
+package csc207.phase1.UTFantasy.AllSkills;
+
+public class ThunderWave extends Skill {
+
+    public ThunderWave() {
+        damage = 5;
+        status = " ";
+        PP = 3;
+        type = "electronic";
+    }
+}
