b4d6e09c19ffc1bd64df930a045b925fe30d6ca5
yimi <yimiswz.wang@mail.utoronto.ca>
2019-10-19 00:07:23 -0400

    Update the ThunderShock

---
+++ phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/ThunderShock.java
@@ -0,0 +1,15 @@
+package csc207.phase1.UTFantasy;
+
+public class ThunderShock extends Skill {
+
+    public ThunderShock() {
+        damage = 1;
+        status = "I know nothing about this";//不知道这个是干嘛的
+        PP = 5;
+    }
+
+
+
+
+
+}
