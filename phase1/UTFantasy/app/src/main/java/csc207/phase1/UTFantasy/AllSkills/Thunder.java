0de301f16ddfcb883a7dfdce0776afab190c19ef
duanliuu <duan-liu@hotmail.com>
2019-10-19 00:16:13 -0400

    thunder

---
+++ phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/AllSkills/Thunder.java
@@ -0,0 +1,10 @@
+package csc207.phase1.UTFantasy.AllSkills;
+
+public class Thunder extends Skill {
+    public Thunder() {
+        damage = 80;
+        PP = 1;
+        status = "available";
+        type = "electronic";
+    }
+}
