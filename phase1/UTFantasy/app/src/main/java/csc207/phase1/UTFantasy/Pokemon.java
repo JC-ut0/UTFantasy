912da25f40b895b6d6a04387ddacbeefac195b2f
joy <yanalsojoy.zhou@mail.utoronto.ca>
2019-10-18 20:51:16 -0400

    Build new classes

--- phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/Pokemon.java
+++ phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/Pokemon.java
@@ -7,7 +7,7 @@ public class Pokemon {
     private String name;
     private int x;
     private int y;
-    private Skill[4] skills;
+    private Skill[] skills = new Skill[4];
     private int level;
     private String status;
     private int HP;
