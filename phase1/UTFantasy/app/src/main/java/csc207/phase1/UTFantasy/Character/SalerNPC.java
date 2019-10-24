a73007b57a22c0a6a26c8521a5fe8f633161e5fe
Yexiong Xu <yexiong.xu@mail.utoronto.ca>
2019-10-23 23:38:05 -0400

    All NPC now have ability method instead of heal, fight or trade.

--- phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/Character/SalerNPC.java
+++ phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/Character/SalerNPC.java
@@ -1,7 +1,5 @@
 package csc207.phase1.UTFantasy.Character;
 
-import csc207.phase1.UTFantasy.Character.NPC;
-
 public class SalerNPC extends NPC {
     public SalerNPC(String name, String gender) {
         super(name, gender);
