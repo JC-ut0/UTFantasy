7c51b2927f0cfce23c34e4c58fc5346dbfddb470
duanliuu <duan-liu@hotmail.com>
2019-10-26 19:32:09 -0400

    skills update

---
+++ phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/AllSkills/HydroPump.java
@@ -0,0 +1,11 @@
+package csc207.phase1.UTFantasy.AllSkills;
+
+public class HydroPump extends Skill {
+    public HydroPump(){
+        name = "Hydro Pump";
+        type = "water";
+        power = 120;
+        pp = 5;
+        effect = "";
+    }
+}
