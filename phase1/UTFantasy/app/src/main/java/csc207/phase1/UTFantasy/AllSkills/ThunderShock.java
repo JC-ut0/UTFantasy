f99c8ef10ec57c1b2c040ed4562d99ec4d819048
quincy <qinxi.yu@mail.utoronto.ca>
2019-10-26 18:18:08 -0400

    "fix to little errors"

--- phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/AllSkills/ThunderShock.java
+++ phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/AllSkills/ThunderShock.java
@@ -3,7 +3,7 @@ package csc207.phase1.UTFantasy.AllSkills;
 public class ThunderShock extends Skill {
 
     public ThunderShock() {
-        damage = 1;
+        power = 1;
         status = "I know nothing about this";//不知道这个是干嘛的
         pp = 5;
         type = "electronic";
