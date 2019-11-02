1d5cbf2d8f602257d4aa3fcc4ae595929a78afc2
duanliuu <duan-liu@hotmail.com>
2019-11-01 22:28:54 -0400

    allskills reformatted

--- phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/Products/Product.java
+++ phase1/UTFantasy/app/src/main/java/csc207/phase1/UTFantasy/Products/Product.java
@@ -44,7 +44,6 @@ public class Product implements Serializable {
     }
 
     @Override
-    @NonNull
     public String toString() {
         return "This is a product.";
     }
@@ -56,8 +55,15 @@ public class Product implements Serializable {
         return profileID;
     }
 
+    /**
+     * @param obj  Product instance
+     * @return true if the product name are the same.
+     */
     @Override
     public boolean equals(@Nullable Object obj) {
+        if (!(obj instanceof Product)){
+            return false;
+        }
         return this.hashCode() == obj.hashCode();
     }
 
