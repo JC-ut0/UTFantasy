c0115da5771d30642575df127f4d9227b6abdb9c
yimi <yimiswz.wang@mail.utoronto.ca>
2019-11-25 15:11:44 -0500

    Update Scoreboard

--- phase2/UTFantasy/app/src/main/java/csc207/phase2/UTFantasy/Activities/ScoreBoardActivity.java
+++ phase2/UTFantasy/app/src/main/java/csc207/phase2/UTFantasy/Activities/ScoreBoardActivity.java
@@ -1,16 +1,39 @@
 package csc207.phase2.UTFantasy.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import csc207.phase2.UTFantasy.Character.Player;
+import csc207.phase2.UTFantasy.IO.UserIO;
 import csc207.phase2.UTFantasy.R;
 
 public class ScoreBoardActivity extends AppCompatActivity {
+
+    /** the unique UserIO */
+    private UserIO userIO = UserIO.getSingletonUserIo();
+
+    List<Player> playerList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_board);
+
+        Intent intent = getIntent();
+
+        // TODO create a list of Player and sort them
+//        Player player = userIO.getUserData().getUser(username).getPlayer();
+
+        ListView listView = findViewById(R.id.scoreBoardViewList);
+        final ScoreBoardAdapter adapter = new ScoreBoardAdapter(this, playerList);
+        listView.setAdapter(adapter);
+
     }
 }
+
